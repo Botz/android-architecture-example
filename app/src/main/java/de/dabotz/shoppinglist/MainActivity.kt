0b16fe2e7de47ea10fdde736d3c137acc98ab835
Andreas Botzler <botzler@jochen-schweizer.de>
2018-01-17 09:42:25 +0100

    use KodeinActivity

--- app/src/main/java/de/dabotz/shoppinglist/MainActivity.kt
+++ app/src/main/java/de/dabotz/shoppinglist/MainActivity.kt
@@ -2,15 +2,12 @@ package de.dabotz.shoppinglist
 
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
-import android.support.v7.app.AppCompatActivity
 import com.github.salomonbrys.kodein.Kodein
-import com.github.salomonbrys.kodein.KodeinInjector
-import com.github.salomonbrys.kodein.android.FragmentActivityInjector
+import com.github.salomonbrys.kodein.android.KodeinFragmentActivity
 import com.github.salomonbrys.kodein.bind
 import com.github.salomonbrys.kodein.instance
 
-class MainActivity : AppCompatActivity(), FragmentActivityInjector {
-    override val injector: KodeinInjector = KodeinInjector()
+class MainActivity : KodeinFragmentActivity() {
 
     override fun provideOverridingModule(): Kodein.Module = Kodein.Module {
         bind<FragmentActivity>("Activity") with instance(this@MainActivity)
@@ -18,12 +15,6 @@ class MainActivity : AppCompatActivity(), FragmentActivityInjector {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initializeInjector()
         setContentView(R.layout.main_layout)
-    }
-
-    override fun onDestroy() {
-        destroyInjector()
-        super.onDestroy()
     }
 }
